00093d026c3d19dd792981abcc9ffab6af35a3ae
MattBelanger321 <mattmbelanger@hotmail.com>
2020-01-05 23:19:33 -0500

    CORRECTED STATE CONSTANTS

--- src/Global.java
+++ src/Global.java
@@ -9,8 +9,8 @@ public interface Global {
     final int WHITE_PLAY = 1;
     final int BLACK_PLAY = 2;
     final int WHITE_CHECK = 3;  //WHITE IS IN CHECK
-    final int BLACK_CHECK = 5;  //BLACK IS IN CHECK
-    final int STALEMATE = 6;
-    final int WHITE_WINS = 7;   //BLACK HAS BEEN CHECKMATED
-    final int BLACK_WINS = 8;   //WHITE HAS BEEN CHECKMATED
+    final int BLACK_CHECK = 4;  //BLACK IS IN CHECK
+    final int STALEMATE = 5;
+    final int WHITE_WINS = 6;   //BLACK HAS BEEN CHECKMATED
+    final int BLACK_WINS = 7;   //WHITE HAS BEEN CHECKMATED
 }
